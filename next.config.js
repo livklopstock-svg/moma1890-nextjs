c65f2e0af746a65f1048708cd85d88aeead266ce
livklopstock-svg <livklopstock@gmail.com>
2025-08-12 12:00:11 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: {
     locales: ['en', 'de', 'es', 'fr'],
     defaultLocale: 'en',
   },
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
+
 module.exports = nextConfig;
